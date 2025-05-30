c4d7e9b29a65d8674352705bff6e36a44dde295c
聖人 渡辺 <k024g0024@g.neec.ac.jp>
2025-05-31 02:42:43 +0900

    commandリスト途中

---
+++ WindowConfig.h
@@ -0,0 +1,24 @@
+#include "Windows.h"
+#include <cstdint>
+
+#pragma once
+
+class WindowConfig
+{
+private:
+
+	WNDCLASS wc{};
+	HWND hwnd;
+
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+	void SetWindowData(const int32_t kClientWidth, const int32_t kClientHeight);
+
+	void DrawWindow(const int32_t kClientWidth, const int32_t kClientHeight);
+
+	HWND GetHwnd() { return hwnd; };
+
+
+};
+
