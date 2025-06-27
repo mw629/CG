97a7d2ec20fa1596d1693374ecd3a92e2a018e1c
mw629 <k024g0024@g.neec.ac.jp>
2025-06-27 10:17:08 +0900

    まだえらーある

---
+++ MatchaEngine/Resource/Load.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Common/GraphicsDevice.h"
+#include "Math/Calculation.h"
+#include <fstream>
+#include <sstream>
+
+/// オブジェクトの読み込み///
+
+MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+
+ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
+
+///テクスチャの読み込み///
+
+DirectX::ScratchImage LoadTexture(const std::string& filePath);
+
+Microsoft::WRL::ComPtr<ID3D12Resource> CreateTextureResource(ID3D12Device* device, const DirectX::TexMetadata& metadata);
+
+Microsoft::WRL::ComPtr<ID3D12Resource> UploadTextureData(ID3D12Resource* texture, const DirectX::ScratchImage& mipImages, ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
