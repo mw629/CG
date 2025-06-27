0eaa9eab660672127332e71f624476009aa68ec8
mw629 <k024g0024@g.neec.ac.jp>
2025-06-27 10:06:38 +0900

    Load.hがまだで実効ができない

--- MatchaEngine/Object/Model.h
+++ MatchaEngine/Object/Model.h
@@ -1,5 +1,55 @@
 #pragma once
+#include <wrl.h>
+#include <d3dx12.h>
+#include <Common/VariableTypes.h>
+
 class Model
 {
+private:
+	ModelData modelData_;
+	Transform transform_;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource_;
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
+	VertexData* vertexData_ = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> wvpDataResource_;
+	TransformationMatrix* wvpData_ = nullptr;
+
+	bool isAlive = true;
+	int kClientWidth = 1280;
+	int kClientHeight = 720;
+public:
+
+
+	Model(ModelData modelData);
+
+	void CreateVertexData(ID3D12Device* device);
+	void CreateWVP(ID3D12Device* device);
+
+	//void SetTexture(ID3D12Device* device);
+
+
+
+	void SetWvp(Transform camera);
+	void SetTransform(Transform transform);
+
+	void CreateModel(ID3D12Device* device);
+
+
+	Transform GetTransform() { return transform_; }
+	void SetPos(Vector3 velocity) { transform_.translate = velocity; }
+
+
+	ModelData GetModelData() { return modelData_; }
+
+	D3D12_VERTEX_BUFFER_VIEW* GetVertexBufferView() { return &vertexBufferView_; }
+
+	ID3D12Resource* GetWvpDataResource() { return wvpDataResource_.Get(); }
+	TransformationMatrix* GetWvpData() { return wvpData_; }
+
+
+
+	void SetAlive(bool flag) { isAlive = flag; }
+	bool GetAlive() { return isAlive; }
 };
 
