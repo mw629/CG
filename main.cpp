957e7483fbc8e8d076714958b9e85c433b9ad535
mw629 <k024g0024@g.neec.ac.jp>
2025-07-16 21:18:33 +0900

    　

--- main.cpp
+++ main.cpp
@@ -463,7 +463,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけばいい
 			command->GetCommandList()->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
 			command->GetCommandList()->SetGraphicsRootSignature(linerootSignature->GetRootSignature());
-			
 
 			//draw.get()->DrawLine(line.get());
 			draw.get()->DrawGrid(grid.get());
