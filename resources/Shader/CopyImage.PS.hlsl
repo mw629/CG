32b49c57a181ed03dec25a1e2faeda2580204fb9
Masat <Masat@MATCHA>
2026-05-13 22:35:40 +0900

    ぼかせてるかわからん

--- resources/Shader/CopyImage.PS.hlsl
+++ resources/Shader/CopyImage.PS.hlsl
@@ -10,24 +10,12 @@ static const float32_t2 kIndex3x3[3][3] =
     { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } }
 };
 
-static const float32_t kPrewittHorizontalKernel[3][3] =
+static const float32_t kKernel3x3[3][3] =
 {
-    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
-    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
-    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f }
 };
-
-static const float32_t kPrewittVerticalKernel[3][3] =
-{
-    { -1.0f / 6.0f, -1.0f / 6.0f, -1.0f / 6.0f },
-    { 0.0f, 0.0f, 0.0f },
-    { 1.0f / 6.0f, 1.0f / 6.0f, 1.0f / 6.0f },
-};
-
-float32_t Luminance(float32_t3 v)
-{
-    return dot(v, float32_t3(0.2125f, 0.7154f, 0.0721f));
-}
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
@@ -38,25 +26,16 @@ PixelShaderOutput main(VertexShaderOutput input)
     gTexture.GetDimensions(width, height);
     float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
     
-    
-    
-    float32_t2 difference = float32_t2(0.0f, 0.0f);
-    for (int32_t x = 0; x < 3; ++x)
+    for(int32_t x = 0; x < 3; ++x)
     {
-        for (int32_t y = 0; y < 3; ++y)
+        for(int32_t y = 0; y < 3; ++y)
         {
-            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
-            float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            float32_t luminance = Luminance(fetchColor);
-            difference.x += luminance * kPrewittHorizontalKernel[x][y];
-            difference.y += luminance * kPrewittVerticalKernel[x][y];
+            float32_t2 texcood = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float32_t3 fetchColor = gTexture.Sample(gSampler, texcood).rgb;
+            output.color.rgb += fetchColor * kKernel3x3[x][y];
         }
     }
-    //変化の長さをウェイトとして合成；ウェイトの決定方法もいろいろと考えれる。
-    float32_t weight = length(difference);
-    weight = saturate(weight * 6.0f); //0.0f～1.0fにしておく
-    output.color.rgb = (1.0f - weight) * gTexture.Sample(gSampler, input.texcoord).rgb;
-    output.color.a = 1.0f;
+    
     
     return output;
 }
