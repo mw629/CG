f39b2da6b4078f46108c2384cbf1c36490d09ebf
matcha <k024g0024@g.neec.ac.jp>
2026-05-13 14:09:19 +0900

    Shaderの修正をした

--- resources/Shader/CopyImage.PS.hlsl
+++ resources/Shader/CopyImage.PS.hlsl
@@ -1,6 +1,6 @@
 #include "CopyImage.hlsli"
 
-Texture2D<float32_t> gTexture : register(t0);
+Texture2D<float32_t4> gTexture : register(t0);
 sampler gSampler : register(s0);
 
 struct PixelShaderOutput
@@ -8,3 +8,9 @@ struct PixelShaderOutput
     float32_t4 color : SV_TARGET;
 };
 
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    output.color = gTexture.Sample(gSampler, input.texcoord);
+    return output;
+}
