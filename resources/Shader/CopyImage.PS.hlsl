2f8edef7865597c5561c38abc5802bf01118f8a0
matcha <k024g0024@g.neec.ac.jp>
2026-05-18 12:58:38 +0900

    ガウスぼかしが少し怪しい

--- resources/Shader/CopyImage.PS.hlsl
+++ resources/Shader/CopyImage.PS.hlsl
@@ -3,7 +3,8 @@
 Texture2D<float32_t4> gTexture : register(t0);
 sampler gSampler : register(s0);
 
-static const uint32_t kKernelSize = 5;
+static const uint32_t kKernelSize = 3;
+static const float32_t PI = 3.14159265f;
 
 static const float32_t2 kIndex3x3[3][3] =
 {
@@ -11,29 +12,13 @@ static const float32_t2 kIndex3x3[3][3] =
     { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
     { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } }
 };
-static const float32_t kKernel3x3[3][3] =
-{
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f }
-};
 
-static const float32_t2 kIndex5x5[5][5] =
+float gauss(float32_t x, float32_t y, float32_t sigma)
 {
-    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
-    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
-    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
-    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
-    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } }
-};
-static const float32_t kKernel5x5[5][5] =
-{
-    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
-    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
-    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
-    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
-    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f }
-};
+    float exponent = -(x * x + y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    return exp(exponent) * rcp(denominator);
+}
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
@@ -41,20 +26,33 @@ PixelShaderOutput main(VertexShaderOutput input)
     output.color.a = 1.0f;
     output.color = gTexture.Sample(gSampler, input.texcoord);
     
-    //uint32_t width, height;
-    //gTexture.GetDimensions(width, height);
-    //float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
+    float32_t weight = 0.0f;
+    float32_t kernel3x3[kKernelSize][kKernelSize];
     
-    //for (int32_t x = 0; x < kKernelSize; ++x)
-    //{
-    //    for (int32_t y = 0; y < kKernelSize; ++y)
-    //    {
-    //        float32_t2 texcood = input.texcoord + kIndex5x5[x][y] * uvStepSize;
-    //        float32_t3 fetchColor = gTexture.Sample(gSampler, texcood).rgb;
-    //        output.color.rgb += fetchColor * kKernel5x5[x][y];
-    //    }
-    //}
+    for (int32_t x = 0; x < kKernelSize; ++x)
+    {
+        for (int32_t y = 0; y < kKernelSize; ++y)
+        {
+            kernel3x3[x][y] = gauss(kIndex3x3[x][y].x, kIndex3x3[x][y].y, 2.0f);
+            weight += kernel3x3[x][y];
+            
+            
+        }
+    }
     
+    uint32_t width, height;
+    gTexture.GetDimensions(width, height);
+    float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
     
+    for (int32_t x = 0; x < kKernelSize; ++x)
+    {
+        for (int32_t y = 0; y < kKernelSize; ++y)
+        {
+            float32_t2 texcood = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float32_t3 fetchColor = gTexture.Sample(gSampler, texcood).rgb;
+            output.color.rgb += fetchColor * kernel3x3[x][y];
+        }
+    }
+        
     return output;
 }
