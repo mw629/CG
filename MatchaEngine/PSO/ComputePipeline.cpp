bd94cf1716bf03810b75e62760be2c6eb8f4e738
Masat <Masat@MATCHA>
2026-05-19 00:58:01 +0900

    CreatePipelineを資料と同じ状態にした

--- MatchaEngine/PSO/ComputePipeline.cpp
+++ MatchaEngine/PSO/ComputePipeline.cpp
@@ -1,9 +1,24 @@
 #include "ComputePipeline.h"
+#include "VariableTypes.h"
 
 void ComputePipeline::CreatePipeline(std::ostream& os, ID3D12Device* device)
 {
-	computePipelineStateDesc_.CS{
-		.pShaderByteCode = 
-	}
+	computePipelineStateDesc_.CS = {
+		.pShaderBytecode = nullptr,
+		.BytecodeLength = 0
+	};
+	computePipelineStateDesc_.pRootSignature = nullptr;
+	computePipelineState_ = nullptr;
+	HRESULT hr = device->CreateComputePipelineState(&computePipelineStateDesc_, IID_PPV_ARGS(&computePipelineState_));
 
+	uavDesc_.Format = DXGI_FORMAT_UNKNOWN;
+	uavDesc_.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
+	uavDesc_.Buffer.FirstElement = 0;
+	uavDesc_.Buffer.NumElements = 0; 
+	uavDesc_.Buffer.CounterOffsetInBytes = 0;
+	uavDesc_.Buffer.Flags = D3D12_BUFFER_UAV_FLAG_NONE;
+	uavDesc_.Buffer.StructureByteStride = sizeof(VertexData);
+
+	//device->CreateUnorderedAccessView(resouce, nullptr, &uavDesc_, descriptor);
+	
 }
--- MatchaEngine/PSO/ComputePipeline.h
+++ MatchaEngine/PSO/ComputePipeline.h
@@ -10,6 +10,8 @@ private:
 	D3D12_COMPUTE_PIPELINE_STATE_DESC computePipelineStateDesc_ = {};
 	Microsoft::WRL::ComPtr<ID3D12PipelineState> computePipelineState_ = nullptr;
 
+	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc_ = {};
+
 public:
 
 	void CreatePipeline(std::ostream& os, ID3D12Device* device);
