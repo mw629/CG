b9150179d6309f5731c946a5630bd314b8ce7a02
matcha <k024g0024@g.neec.ac.jp>
2026-05-22 16:58:57 +0900

    一応 Dissolveはいれたが実行できない

--- resources/Shader/CopyImage.PS.hlsl
+++ resources/Shader/CopyImage.PS.hlsl
@@ -11,12 +11,14 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     
     float32_t mask = gMaskTexture.Sample(gSampler, input.texcoord).r;
+    float32_t edge = 1.0f - smoothstep(0.5f, 0.53f, mask);
+
     
     if (mask <= 0.5f)
     {
         discard;
     }
-    output.color = gTexture.Sample(gSampler, input.texcoord);
+    output.color.rgb = edge * float32_t3(1.0f, 0.4f, 0.3f);
     
     return output;
 }
