a54d77ce4303c2add98559d42feda8459e09a091
matcha <k024g0024@g.neec.ac.jp>
2026-06-08 19:42:11 +0900

    ぼかしの調整をした

--- MatchaEngine/Graphics/Render/PostEffect.cpp
+++ MatchaEngine/Graphics/Render/PostEffect.cpp
@@ -8,6 +8,28 @@ PostEffect::Type PostEffect::activeType_ = PostEffect::Type::Normal;
 std::string PostEffect::activeShaderName_ = "CopyShader";
 
 PostEffect::PostEffect() {
+	for (int y = 0; y < 3; ++y) {
+		for (int x = 0; x < 3; ++x) {
+			kernel3x3_[y][x] = 1.0f / 9.0f;
+		}
+	}
+	for (int y = 0; y < 5; ++y) {
+		for (int x = 0; x < 5; ++x) {
+			kernel5x5_[y][x] = 1.0f / 25.0f;
+		}
+	}
+	for (int y = 0; y < 3; ++y) {
+		for (int x = 0; x < 3; ++x) {
+			index3x3_[y][x][0] = (float)(x - 1);
+			index3x3_[y][x][1] = (float)(y - 1);
+		}
+	}
+	for (int y = 0; y < 5; ++y) {
+		for (int x = 0; x < 5; ++x) {
+			index5x5_[y][x][0] = (float)(x - 2);
+			index5x5_[y][x][1] = (float)(y - 2);
+		}
+	}
 }
 
 PostEffect::~PostEffect() {
@@ -26,6 +48,30 @@ void PostEffect::Initialize() {
 		cbData_->ratio = ratio_;
 		cbData_->value1 = value1_;
 		cbData_->value2 = value2_;
+		cbData_->blurStrength = blurStrength_;
+		cbData_->kernelSize = kernelSize_;
+		for (int y = 0; y < 3; ++y) {
+			for (int x = 0; x < 3; ++x) {
+				cbData_->kernel3x3[y][x].v = kernel3x3_[y][x];
+			}
+		}
+		for (int y = 0; y < 5; ++y) {
+			for (int x = 0; x < 5; ++x) {
+				cbData_->kernel5x5[y][x].v = kernel5x5_[y][x];
+			}
+		}
+		for (int y = 0; y < 3; ++y) {
+			for (int x = 0; x < 3; ++x) {
+				cbData_->index3x3[y][x].v[0] = index3x3_[y][x][0];
+				cbData_->index3x3[y][x].v[1] = index3x3_[y][x][1];
+			}
+		}
+		for (int y = 0; y < 5; ++y) {
+			for (int x = 0; x < 5; ++x) {
+				cbData_->index5x5[y][x].v[0] = index5x5_[y][x][0];
+				cbData_->index5x5[y][x].v[1] = index5x5_[y][x][1];
+			}
+		}
 	}
 
 	SetMaskTexturePath(maskTexturePath_);
@@ -41,6 +87,30 @@ void PostEffect::Update(float deltaTime) {
 		cbData_->ratio = ratio_;
 		cbData_->value1 = value1_;
 		cbData_->value2 = value2_;
+		cbData_->blurStrength = blurStrength_;
+		cbData_->kernelSize = kernelSize_;
+		for (int y = 0; y < 3; ++y) {
+			for (int x = 0; x < 3; ++x) {
+				cbData_->kernel3x3[y][x].v = kernel3x3_[y][x];
+			}
+		}
+		for (int y = 0; y < 5; ++y) {
+			for (int x = 0; x < 5; ++x) {
+				cbData_->kernel5x5[y][x].v = kernel5x5_[y][x];
+			}
+		}
+		for (int y = 0; y < 3; ++y) {
+			for (int x = 0; x < 3; ++x) {
+				cbData_->index3x3[y][x].v[0] = index3x3_[y][x][0];
+				cbData_->index3x3[y][x].v[1] = index3x3_[y][x][1];
+			}
+		}
+		for (int y = 0; y < 5; ++y) {
+			for (int x = 0; x < 5; ++x) {
+				cbData_->index5x5[y][x].v[0] = index5x5_[y][x][0];
+				cbData_->index5x5[y][x].v[1] = index5x5_[y][x][1];
+			}
+		}
 	}
 }
 
@@ -75,7 +145,18 @@ void PostEffect::ImGuiWindow() {
 			cbData_->ratio = ratio_;
 			cbData_->value1 = value1_;
 			cbData_->value2 = value2_;
+			cbData_->blurStrength = blurStrength_;
+			cbData_->kernelSize = kernelSize_;
 		}
+
+		if (activeType_ == Type::Smoothing || activeType_ == Type::GaussianFilter) {
+			ImGui::SliderFloat("Blur Strength##PostEffect", &blurStrength_, 0.0f, 10.0f);
+
+			ImGui::Text("Kernel Size:"); ImGui::SameLine();
+			ImGui::RadioButton("3x3##KernelSize", &kernelSize_, 3); ImGui::SameLine();
+			ImGui::RadioButton("5x5##KernelSize", &kernelSize_, 5);
+		}
+
 
 		if (activeType_ == Type::Dissolve) {
 			char buffer[256];
